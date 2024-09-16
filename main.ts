0f542c59faa418b64b533423cd7a773ffb5753cd
Zara Yousuf Khan <zaara.khan1795.zy@gmail.com>
2024-09-16 18:02:22 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,56 @@
+function orderProm (){
+    return new Promise ((resolve => {
+        setTimeout(() => {
+            console.log("Adding Items in your Cart 🛒")
+           resolve("")
+        },2000)
+    }))
+
+}
+
+function orderconfirmationProm (){
+    return new Promise ((resolve => {
+        setTimeout(() => {
+            console.log("Your order is Confirmed 🛍")
+            resolve("")
+           
+        },3000)
+    }))
+}
+
+function orderprocessingProm (){
+    return new Promise ((resolve => {
+        setTimeout(() => {
+            console.log("Your Order is in Process")
+            resolve("")
+        },4000)
+    }))
+}
+
+function orderoutfordeliveryProm (){
+    return new Promise ((resolve => {
+        setTimeout(()=> {
+            console.log("Your order is Out for Delivery")
+            console.log("Your order is on its way")
+            resolve("")
+        },5000)
+    }))
+}
+
+function orderdeliveryProm (){
+    return new Promise((resolve => {
+        setTimeout(() => {
+            console.log("Your Order is Delivered")
+            console.log("Thankyou for Placing your Order")
+            resolve("")
+        },6000)
+    }))
+}
+
+let result = await orderProm()
+let result1 = await orderconfirmationProm()
+let result2 = await orderprocessingProm()
+let result3 = await orderoutfordeliveryProm()
+let result4 = await orderdeliveryProm()
+
+orderProm().then(orderconfirmationProm).then(orderprocessingProm).then(orderoutfordeliveryProm).then(orderdeliveryProm)
